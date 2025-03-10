7d4b9caa7495f4c9bb14bf50e109216d46817e79
malopez19 <lopezmateojurado@gmail.com>
2025-03-10 18:15:16 -0500

    refactor home view to use layout and add custom styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Laravel 12</title>
-</head>
-<body>
-    <div class="max-w-4xl mx-auto px-4">
-        <h1 class="text-2xl font-bold text-center">Bienvenido al home</h1>
+@extends('layouts.app')
 
-        <x-alert type="danger" class="mb-10">
-            <x-slot name="title">Alerta de información</x-slot>
-            componentes anonimos
-        </x-alert>
+@section('title', 'Home')
 
-        <x-alert2 type="success" class="mb-10">
-            <x-slot name="title">Alerta de información</x-slot>
-            componentes de clase
-        </x-alert2>
+@push('css')
+    <style>
+        body{
+            background-color: rgb(5, 5, 114);
+        }
 
-        <p>Hola mund0</p>
-    </div>
-</body>
-</html>
+        p, h1{
+            color: white;
+        }
+    </style>
+@endpush
+
+@section('content')
+    <h1 class="text-2xl font-bold text-center">Bienvenido al home</h1>
+
+    <x-alert type="danger" class="mb-10">
+        <x-slot name="title">Alerta de información</x-slot>
+        componentes anonimos
+    </x-alert>
+
+    <x-alert2 type="success" class="mb-10">
+        <x-slot name="title">Alerta de información</x-slot>
+        componentes de clase
+    </x-alert2>
+
+    <p>Hola mund0</p>
+@endsection
+
+
+
+
